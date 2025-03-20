973bc27ce8a4a8773e991ad1a967040b5f476a86
Sebastián Zapateiro <sebastxsd@gmail.com>
2025-03-20 15:49:34 -0500

    dev: variable name changed

--- segundo.py
+++ segundo.py
@@ -1,4 +1,49 @@
-t = {}
+""""
+Registro de Constructoras
+
+Desarrolla un programa que permita registrar constructoras. Para cada constructora, se
+deben solicitar los siguientes datos por teclado: 
+Nombre de la constructora
+Cantidad de proyectos vigentes (debe ser un número entero mayor o igual a 0)
+Cantidad de ventas por cada proyecto vigente (debe ser un número entero mayor o igual a 0)
+El programa debe cumplir con los siguientes requisitos:
+
+1. Validación de datos: No se deben aceptar valores vacíos ni datos inválidos (por
+ejemplo, valores negativos o caracteres en campos numéricos).
+
+2. Uso de arreglos: Los datos de las constructoras y sus proyectos deben almacenarse
+en una estructura adecuada (por ejemplo, listas o diccionarios). 
+
+3. Salida de datos: Una vez ingresada la información de al menos una constructora, mostrar un resumen con:
+
+o Nombre de la constructora
+o Total de proyectos vigentes
+o Total de ventas sumando todas las ventas de los proyectos
+
+Opcional: Permitir el ingreso de varias constructoras y finalizar el programa cuando
+el usuario lo indique. Exportación de datos: Al finalizar, permitir al usuario exportar los datos ingresados
+en un archivo de texto (.txt) con el siguiente formato:
+Nombre: Construcciones XYZ
+Total de proyectos vigentes: 2
+Total de ventas: 15
+---
+Nombre: Construcciones ABC
+Total de proyectos vigentes: 3
+Total de ventas: 25
+--- Ejemplo de
+"""
+
+"""
+ {
+    builderName: "CONSTRUCTORA BOLIVAR",
+    projectNumber: 56,
+    salesProject: 1
+ }
+
+"""
+# Diccionario de constructoras 
+builder = {}
+
 
 
 while True:
@@ -18,7 +63,7 @@ while True:
                 nombre = input("Ingresa el nombre de la Constructora: ")
                 cantidad =  (int(input("Ingresa la cantidad de proyectos vigentes de esta Constructora: ")))
                 ventas = input("ingresa las ventas de la constructora: ")
-                t[id_constructora] = {
+                builder[id_constructora] = {
                     "nombre": nombre,
                     "cantidad": cantidad,
                     "ventas": ventas,
@@ -40,7 +85,7 @@ while True:
                 print(f"Ventas registradas:1",{ventas}+ {ventas})
     elif opcion == 4:
         
-        print(t,"estos son los proyectos ingresados")    
+        print(builder,"estos son los proyectos ingresados")    
         
     elif opcion == 5:
         
@@ -49,4 +94,3 @@ while True:
     
     else :
          print("Ingresa una opcion valida ")
-         

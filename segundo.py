885b034e6bc511cf5674c27d7afacecf963c1464
AndresSepulveda <andres.sepulveda.hdez@gmail.com>
2025-03-27 10:36:08 -0500

    actualizacion2

--- segundo.py
+++ segundo.py
@@ -42,65 +42,59 @@ Total de ventas: 25
 
 """
 # Diccionario de constructoras 
-builder = {}
+bbuilder = {}
 
-def validator(input):
-       option = 0
-       while True:
+def validator(input_text):
+    option = 0
+    while True:
         try:
-             option = int(input)
-             if isinstance(option,int):
-                  if option <= 0:
-                       print("Ingresa un número mayor a 0")
-                  else:
-                   break
-        except:
-               print("Por favor, ingresa un número") 
-        return option       
-
+            option = int(input_text)
+            if option <= 0:
+                print("Ingresa un número mayor a 0")
+            else: 
+                return option
+        except ValueError:
+            print("Por favor, ingresa un número válido")
+        input_text = input("Introduce nuevamente el número:")  
 
 while True:
-
     print("Bienvenido al programa de registro de Constructoras")
     print("1.- Agregar Constructora")
-    print("2.- Mostrar  cantidad de proyectos de las Constructoras")
-    print("3.- ventas de proyectos por  Constructora")
-    print("4.- ver proyectos guardados por Constructoras")
-    print("5.- salir del programa ")
- 
-    while True:
-        opcion = input("Introduce el número de la opción que deseas: ")        
-        try:
-             opcion = int(opcion)
-             if isinstance(opcion,int):
-                  if opcion <= 0:
-                       print("Ingresa un número mayor a 0")
-                  else:
-                   break
-        except:
-               print("Por favor, ingresa un número")
-          
-    """
-    if opcion == 1:
-        id_constructora =(int(input("Ingresa un ID único para esta Constructora: ")))
-        print("Guardaste el id")
-       
-            
+    print("2.- Mostrar cantidad de proyectos de las Constructoras")
+    print("3.- Ventas de proyectos por Constructora")
+    print("4.- Ver proyectos guardados por Constructoras")
+    print("5.- Salir del programa")
+
+    opcion = input("Introduce el número de la opción que deseas: ")        
+    try:
+        opcion = validator(opcion)
+        
+        if opcion == 1:
+            constructora = input("Ingresa el nombre de la constructora: ")
+            proyecto = input("Ingresa proyecto de la constructora: ")
+            venta = input("Ingresa venta de la constructora: ")
+            id_proyecto = len(builder) + 1
+            builder[id_proyecto] = {
+                "constructora": constructora,
+                "Proyecto": proyecto,
+                "venta": venta,
+            }
+            print("Constructora y sus datos guardados correctamente")
+
+        elif opcion == 2:
+            print("Los proyectos registrados son:", len(builder))
+
         elif opcion == 3:
-        
-            if ventas >= 0: 
-                print("No existen ventas registradas")
-            else:
-                print(f"Ventas registradas:1",{ventas}+ {ventas})
-         elif opcion == 4:
-        
-        print(builder,"estos son los proyectos ingresados")    
-        
-    elif opcion == 5:
-        
-        print("saliste del programa ")   
-        break
-    
-else :
-         print("Ingresa una opcion valida ")"
-         """
+            for id_proyecto, datos in builder.items():
+                print(f"ID: {id_proyecto}, Ventas: {datos['venta']}")
+
+        elif opcion == 4:
+            for id_proyecto, datos in builder.items():
+                print(f"ID: {id_proyecto}, Datos: {datos}")
+
+        elif opcion == 5:
+            print("Saliendo del programa...")
+            break
+
+    except Exception as e:
+        print(f"Error inesperado: {e}")
